627d3f4f88b9dab99beecb3e4d39926083dec9ec
Simon Tucker <simon@brotherlogic@gmail>
2019-11-03 09:57:12 -0800

    Adds ability to force process a record.

--- recordprocessapi.go
+++ recordprocessapi.go
@@ -1,8 +1,12 @@
 package main
 
-import "golang.org/x/net/context"
+import (
+	"fmt"
 
-import pb "github.com/brotherlogic/recordprocess/proto"
+	"golang.org/x/net/context"
+
+	pb "github.com/brotherlogic/recordprocess/proto"
+)
 
 // GetScore gets the score for an instance
 func (s *Server) GetScore(ctx context.Context, req *pb.GetScoreRequest) (*pb.GetScoreResponse, error) {
@@ -15,4 +19,20 @@ func (s *Server) GetScore(ctx context.Context, req *pb.GetScoreRequest) (*pb.Get
 	}
 
 	return response, nil
+}
+
+// Force a process of a specific record
+func (s *Server) Force(ctx context.Context, req *pb.ForceRequest) (*pb.ForceResponse, error) {
+	record, err := s.getter.getRecord(ctx, req.InstanceId)
+	if err != nil {
+		return nil, err
+	}
+
+	update, result := s.processRecord(record)
+	if update != nil {
+		err := s.getter.update(ctx, update)
+		return &pb.ForceResponse{Result: update}, err
+	}
+
+	return nil, fmt.Errorf("Unable to process: %v", result)
 }
--- recordprocessapi_test.go
+++ recordprocessapi_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"testing"
 
+	pbgd "github.com/brotherlogic/godiscogs"
 	pbrc "github.com/brotherlogic/recordcollection/proto"
 	pb "github.com/brotherlogic/recordprocess/proto"
 )
@@ -25,5 +26,35 @@ func TestGetScore(t *testing.T) {
 
 	if len(scores.GetScores()) != 1 {
 		t.Errorf("Bad scores: %v", scores)
+	}
+}
+
+func TestForceUpdate(t *testing.T) {
+	s := InitTest()
+	s.getter = &testGetter{rec: &pbrc.Record{Release: &pbgd.Release{FolderId: 1727264, Labels: []*pbgd.Label{&pbgd.Label{Name: "blah"}}}, Metadata: &pbrc.ReleaseMetadata{Category: pbrc.ReleaseMetadata_PARENTS, GoalFolder: 242017, Cost: 12}}}
+	_, err := s.Force(context.Background(), &pb.ForceRequest{InstanceId: 1234})
+
+	if err != nil {
+		t.Errorf("Error in proc: %v", err)
+	}
+}
+
+func TestForceUpdateFailGet(t *testing.T) {
+	s := InitTest()
+	s.getter = &testGetter{getFail: true}
+	_, err := s.Force(context.Background(), &pb.ForceRequest{InstanceId: 1234})
+
+	if err == nil {
+		t.Errorf("No fail on get")
+	}
+}
+
+func TestForceUpdateFailProcess(t *testing.T) {
+	s := InitTest()
+	s.getter = &testGetter{rec: &pbrc.Record{Metadata: &pbrc.ReleaseMetadata{Category: pbrc.ReleaseMetadata_PURCHASED}, Release: &pbgd.Release{Labels: []*pbgd.Label{&pbgd.Label{Name: "Label"}}, FolderId: 1}}}
+	_, err := s.Force(context.Background(), &pb.ForceRequest{InstanceId: 1234})
+
+	if err == nil {
+		t.Errorf("No fail on process")
 	}
 }
