ac59e125eb097b6e7491e77692a62975816ce5dc
simon <brotherlogic@gmail.com>
2021-06-22 10:36:24 -0700

    Move on sold date update

--- recordprocessutils.go
+++ recordprocessutils.go
@@ -137,7 +137,7 @@ func (s *Server) processRecord(ctx context.Context, r *pbrc.Record) (pbrc.Releas
 
 	if (r.GetMetadata().Category == pbrc.ReleaseMetadata_LISTED_TO_SELL ||
 		r.GetMetadata().Category == pbrc.ReleaseMetadata_SOLD_OFFLINE ||
-		r.GetMetadata().Category == pbrc.ReleaseMetadata_STALE_SALE) && r.GetMetadata().SaleState == pbgd.SaleState_SOLD {
+		r.GetMetadata().Category == pbrc.ReleaseMetadata_STALE_SALE) && (r.GetMetadata().SaleState == pbgd.SaleState_SOLD || r.GetMetadata().GetSoldDate() > 0) {
 		return pbrc.ReleaseMetadata_SOLD_ARCHIVE, -1, "Sold"
 	}
 
